dc0405175ecc8986d1dd2799faa188159c70bbe7
Dipankar <dipankar15-11137@diu.edu.bd>
2022-08-27 23:17:52 +0600

    update quantity

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +11,57 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.09jmf63.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // console.log("database connect");
+    const appleProductsCollection = client
+      .db("city-electronics")
+      .collection("apple-products");
+
+    app.get("/appleProducts", async (req, res) => {
+      const query = {};
+      const cursor = appleProductsCollection.find(query);
+      const mainProducts = await cursor.toArray();
+      res.send(mainProducts);
+    });
+    app.get("/appleProducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const products = await appleProductsCollection.findOne(query);
+      res.send(products);
+    });
+    // Update and Delivery
+    app.put("/appleProducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateQuantity = req.body;
+      // console.log(updateQuantity);
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity: updateQuantity.quantity,
+        },
+      };
+      const result = await appleProductsCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Running city Server");
